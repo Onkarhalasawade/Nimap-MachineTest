4f37040b5cdab033b88a20d2c444462a8cc8f65a
Onkarhalasawade <95295399+Onkarhalasawade@users.noreply.github.com>
2024-11-06 20:31:57 +0530

    Add files via upload

---
+++ nimap-machine-test-main/demo/src/test/java/com/Nimap/demo/NimapMachineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nimap.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimapMachineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
